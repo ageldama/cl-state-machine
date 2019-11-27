9e2a47f12af0d80ffabee795ef76333fb4ab45f5
Jong-Hyouk Yun <ageldama@gmail.com>
2019-11-27 14:01:37 +0900

    WIP

--- src/utils.lisp
+++ src/utils.lisp
@@ -6,3 +6,20 @@
         (append (gethash key ht '()) (list item))))
 
 
+
+
+(defmacro append-f (place &rest lists)
+  `(setf ,place (append ,place ,@lists)))
+
+
+(defmacro plist-append-if-ok-f (place key val)
+  `(when ,val (append-f ,place (list ,key ,val))))
+
+(defun plist-merge-if-ok-f (place-plist a-plist)
+  (assert (evenp (length a-plist)))
+  (loop :for idx :from 0 :below (length a-plist) :by 2
+        :for k := (elt a-plist idx)
+        :for v := (elt a-plist (1+ idx))
+        :do (plist-append-if-ok-f place-plist k v)))
+
+
--- test/utils.lisp
+++ test/utils.lisp
@@ -12,3 +12,22 @@
     (is (= 2 (hash-table-count ht)))
     (is (and (equal '(a b) (gethash :a ht))
              (equal '(x) (gethash :b ht))))))
+
+(test append-f
+  (let ((l '()))
+    (cl-state-machine::append-f l)
+    (is (equal l '())))
+  (let ((l '(:a)))
+    (cl-state-machine::append-f l '(:b))
+    (is (equal l '(:a :b)))
+    (cl-state-machine::append-f l '(:c) '(:d))
+    (is (equal l '(:a :b :c :d)))))
+
+
+(test plist-append-if-ok-f
+  (let ((l '(:banana :yellow)))
+    (cl-state-machine::plist-append-if-ok-f l :apple :red)
+    (cl-state-machine::plist-append-if-ok-f l :pineaple nil)
+    (cl-state-machine::plist-append-if-ok-f l :anana :green)
+    (is (equal l '(:banana :yellow :apple :red :anana :green)))))
+
