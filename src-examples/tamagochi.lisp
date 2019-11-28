342a4b86da983d9fd230d28e16c8b10dba8343a5
Jong-Hyouk Yun <ageldama@gmail.com>
2019-11-29 08:26:17 +0900

    Example: tamagochi DONE.

--- src-examples/tamagochi.lisp
+++ src-examples/tamagochi.lisp
@@ -15,7 +15,10 @@
            :initform 5)
    (turns :initarg :turns
           :accessor turns
-          :initform 0)))
+          :initform 0)
+   (cause-of-death :initarg :cause-of-death
+                   :accessor cause-of-death
+                   :initform nil)))
 
 (defmacro with-tamagochi-status (binding-name a-state-transition &rest body)
   (let ((a-state-machine# (gensym)))
@@ -31,33 +34,120 @@
           (t nil))))
 
 (defun is-alive?-after-hook (a-state-transition &rest args)
-  (when-let (cause-of-death (need-to-die? a-state-transition))
-    (let ((a-state-machine (state-transition-state-machine a-state-transition)))
-      ;; TODO:
-      (jump! a-state-machine :dead))))
+  (declare (ignore args))
+  (with-tamagochi-status a-status a-state-transition
+    (when-let (cause (need-to-die? a-state-transition))
+      (let ((a-state-machine (state-transition-state-machine a-state-transition)))
+        (setf (cause-of-death a-status) cause)
+        (jump! a-state-machine :dead)))))
 
 (defun inc-turns-after-hook (a-state-transition &rest args)
+  (declare (ignore args))
   (with-tamagochi-status status a-state-transition
     (incf (turns status))))
 
-(defun make-tamagochi-state-machine ()
-  (let* ((a-status (make-instance 'tamagochi-status)))
-    (state-machine-of `(:current-state :home
-                        :datum ,a-status)
-                      (`(:state :home)
-                       `(:state :work)
-                        `(:state :dead :terminal t))
-                      (`(:from :home :to :work
-                         :event :go-to-work)
-                        `(:from :work :to :home
-                          :event :go-home)
-                        `(:from :home :to :home
-                          :event :eat)
-                        `(:from :home :to :home
-                          :event :sleep)
-                        `(:from :home :to :home
-                          :event :shower)))))
+(defun go-to-work-before-hook (a-state-transition &rest args)
+  (declare (ignore args))
+  (with-tamagochi-status status a-state-transition
+    (when (>= 0 (money status)) (reject-transition! :datum :no-money))
+    ;; else
+    (incf (money status) 5)
+    (decf (food status) 1)
+    (decf (hygiene status) 1)
+    (decf (health status) 1)))
+
+(defun go-home-before-hook (a-state-transition &rest args)
+  (declare (ignore args))
+  (with-tamagochi-status status a-state-transition
+    (when (>= 0 (money status)) (reject-transition! :datum :no-money))
+    ;; else
+    (decf (money status) 1)
+    (decf (food status) 1)
+    (decf (hygiene status) 1)
+    (incf (health status) 1)))
+
+(defun eat-before-hook (a-state-transition &rest args)
+  (declare (ignore args))
+  (with-tamagochi-status status a-state-transition
+    (when (>= 0 (money status)) (reject-transition! :datum :no-money))
+    ;; else
+    (decf (money status) 2)
+    (incf (food status) 3)
+    (decf (hygiene status) 1)
+    (incf (health status) 2)))
+
+(defun sleep-before-hook (a-state-transition &rest args)
+  (declare (ignore args))
+  (with-tamagochi-status status a-state-transition
+    (decf (food status) 1)
+    (decf (hygiene status) 1)
+    (incf (health status) 3)))
+
+(defun shower-before-hook (a-state-transition &rest args)
+  (declare (ignore args))
+  (with-tamagochi-status status a-state-transition
+    (decf (food status) 1)
+    (incf (hygiene status) 3)
+    (incf (health status) 1)))
+
+(defun make-tamagochi-state-machine (a-tamagochi-status)
+  (state-machine-of `(:current-state :home
+                      :datum ,a-tamagochi-status
+                      :after-hooks (list ,#'is-alive?-after-hook
+                                         ,#'inc-turns-after-hook))
+                    (`(:state :home)
+                      `(:state :work)
+                      `(:state :dead :terminal t))
+                    (`(:from :home :to :work
+                       :event :go-to-work
+                       :before-hooks (list ,#'go-to-work-before-hook))
+                      `(:from :work :to :home
+                        :event :go-home
+                        :before-hooks (list ,#'go-home-before-hook))
+                      `(:from :home :to :home
+                        :event :eat
+                        :before-hooks (list ,#'eat-before-hook))
+                      `(:from :home :to :home
+                        :event :sleep
+                        :before-hooks (list ,#'sleep-before-hook))
+                      `(:from :home :to :home
+                        :event :shower
+                        :before-hooks (list ,#'shower-before-hook)))))
+
+(defun display-status (a-tamagochi-status a-state-machine)
+  (terpri)
+  ;; alive?
+  (when (eq :dead (current-state a-state-machine))
+    (format t "Your tamagochi is dead (Cause: ~a)~%"
+            (cause-of-death a-tamagochi-status)))
+  ;; numbers..
+  (format t "Status: Money=~a Food=~a Hygiene=~a Health=~a / Turns=~a~%"
+          (money a-tamagochi-status)
+          (food a-tamagochi-status)
+          (hygiene a-tamagochi-status)
+          (health a-tamagochi-status)
+          (turns a-tamagochi-status)))
+
+(defun prompt (a-tamagochi-status a-state-machine)
+  (declare (ignore a-tamagochi-status))
+  (terpri)
+  (format t "Choice:~% ~{~s ~}~%"
+          (append (list :quit)
+                  (possible-events a-state-machine)))
+  (princ ">>> ")
+  (read))
 
 (defun run-tamagochi ()
-  ;; TODO
-  )
+  (let* ((a-status (make-instance 'tamagochi-status))
+         (a-state-machine (make-tamagochi-state-machine a-status))
+         (choice nil))
+    (loop :named repl-loop
+          :do (progn (display-status a-status a-state-machine)
+                     (setf choice (prompt a-status a-state-machine))
+                     (when (eq :quit choice)
+                       (format t "~%~%Bye!~%")
+                       (return-from repl-loop))
+                     (if (member choice (possible-events a-state-machine))
+                         (trigger! a-state-machine choice)
+                         (format t "~a ???~%" choice))))))
+
